40d9107b17dc318f5a93f771618c2737a684fca0
AnnJBarr <annbarr@hotmail.com>
2019-09-30 16:33:15 +0100

    initial commit of this repo completed previously

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,40 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+
+function makeHalfPrice(price) {
+    return price - (0.5*price);
+}
+
+
+function countBooks(bookTitles) {
+    return bookTitles.length;
+}
+
+
+function isInStock(book) {
+    if (book.quantity > 0) {  
+        return true 
+    } else {
+        return false;
+    }  
+}
+
+
+function getTotalOrderPrice(price, quantity) {
+    return price * quantity * 1.2;
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,68 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+
 });
+
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 75 when passed 150", () => {
+    expect(utils.makeHalfPrice(150)).toBe(75);
+  });
+});
+
+
+describe("utils.countBooks", () => {
+  test("returns 5 when passed an array of 5 titles", () => {
+    expect(utils.countBooks(["Title1", "Title2", "Title3", "Title4", "Title6"])).toBe(5);
+  });
+});
+
+
+describe("utils.isInStock", () => {
+  test("returns true if the title is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+  
+    expect(utils.isInStock(book)).toBe(true);
+  });
+
+  test("returns false if the title is not in stock", () => {
+    const book = {
+      title: "The Mud Diaries",
+      author: "Martin Morris",
+      yearOfPublication: 2006,
+      quantity: 0
+    };
+  
+    expect(utils.isInStock(book)).toBe(false);
+  });
+});
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns 43.2 when passed price is 18 and quantity is 2", () => {
+    expect(utils.getTotalOrderPrice(1800, 2)).toBe(4320);
+    // to avoid rounding error causing the test to fail £18 converted to 1800pence. Floating point arithmetic quirks of JS
+  });
+
+  test("returns 96 when passed price is 8 and quantity is 10", () => {
+    expect(utils.getTotalOrderPrice(8, 10)).toBe(96);
+  });
+});
+
+
